5aeaff99748541afe8d0207272e5dc0ebbb39ea5
M.V <marco.mov98@gmail.com>
2025-01-16 08:49:39 +0000

    fourth commit peppo sad

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -4,7 +4,7 @@ import "../../styles/index.css";
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by Me!Marco!:3
+			Made with <i className="fa fa-heart text-danger" /> by Marco-MOV :3
 		</p>
 	</footer>
 );
